bf76aef3365a918fc9735b0f6a6c2f74ffc9788c
mumer2 <umermalik8280@gmail.com>
2025-08-05 17:34:53 +0500

    login

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -36,7 +36,7 @@ exports.handler = async (event) => {
     const db = await connectDB();
     const users = db.collection('users');
 
-    // Detect whether input is a phone number or an email
+    // Determine query by input type
     let query = {};
     if (/^\d{10,15}$/.test(input)) {
       query = { phone: input };
@@ -52,7 +52,7 @@ exports.handler = async (event) => {
 
     const user = await users.findOne(query);
 
-    if (!user || !(await bcrypt.compare(password, user.password || user.passwordHash))) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return {
         statusCode: 401,
         headers: corsHeaders(),
@@ -60,6 +60,7 @@ exports.handler = async (event) => {
       };
     }
 
+    // Generate token
     const token = generateToken({
       id: user._id,
       email: user.email || '',
@@ -77,7 +78,9 @@ exports.handler = async (event) => {
           name: user.name,
           email: user.email || '',
           phone: user.phone || '',
-          balance: user.balance || 0,
+          points: user.points || 0,
+          referralCode: user.referralCode || '',
+          referredBy: user.referredBy || '',
         },
       }),
     };
@@ -99,6 +102,110 @@ function corsHeaders() {
     'Content-Type': 'application/json',
   };
 }
+
+
+
+// const bcrypt = require('bcryptjs');
+// const connectDB = require('./utils/db');
+// const { generateToken } = require('./utils/auth');
+
+// exports.handler = async (event) => {
+//   if (event.httpMethod === 'OPTIONS') {
+//     return {
+//       statusCode: 200,
+//       headers: corsHeaders(),
+//       body: '',
+//     };
+//   }
+
+//   if (event.httpMethod !== 'POST') {
+//     return {
+//       statusCode: 405,
+//       headers: corsHeaders(),
+//       body: JSON.stringify({ message: 'Method Not Allowed' }),
+//     };
+//   }
+
+//   try {
+//     const body = JSON.parse(event.body);
+//     const password = body.password;
+//     const rawInput = body.email || body.phone;
+
+//     if (!rawInput || !password) {
+//       return {
+//         statusCode: 400,
+//         headers: corsHeaders(),
+//         body: JSON.stringify({ message: 'Email or phone and password are required' }),
+//       };
+//     }
+
+//     const input = rawInput.trim();
+//     const db = await connectDB();
+//     const users = db.collection('users');
+
+//     // Detect whether input is a phone number or an email
+//     let query = {};
+//     if (/^\d{10,15}$/.test(input)) {
+//       query = { phone: input };
+//     } else if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input)) {
+//       query = { email: input.toLowerCase() };
+//     } else {
+//       return {
+//         statusCode: 400,
+//         headers: corsHeaders(),
+//         body: JSON.stringify({ message: 'Invalid email or phone format' }),
+//       };
+//     }
+
+//     const user = await users.findOne(query);
+
+//     if (!user || !(await bcrypt.compare(password, user.password || user.passwordHash))) {
+//       return {
+//         statusCode: 401,
+//         headers: corsHeaders(),
+//         body: JSON.stringify({ message: 'Invalid credentials' }),
+//       };
+//     }
+
+//     const token = generateToken({
+//       id: user._id,
+//       email: user.email || '',
+//       phone: user.phone || '',
+//       name: user.name,
+//     });
+
+//     return {
+//       statusCode: 200,
+//       headers: corsHeaders(),
+//       body: JSON.stringify({
+//         token,
+//         user: {
+//           _id: user._id,
+//           name: user.name,
+//           email: user.email || '',
+//           phone: user.phone || '',
+//           balance: user.balance || 0,
+//         },
+//       }),
+//     };
+//   } catch (err) {
+//     console.error('❌ Login error:', err);
+//     return {
+//       statusCode: 500,
+//       headers: corsHeaders(),
+//       body: JSON.stringify({ message: 'Login failed', error: err.message }),
+//     };
+//   }
+// };
+
+// function corsHeaders() {
+//   return {
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//     'Access-Control-Allow-Methods': 'POST, OPTIONS',
+//     'Content-Type': 'application/json',
+//   };
+// }
 
 
 
