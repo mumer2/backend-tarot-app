d855220f4c92a32f7ff8503d93cde15229e5da3e
mumer2 <umermalik8280@gmail.com>
2025-08-22 10:31:31 +0500

    alipay

--- netlify/functions/alipay-pay.js
+++ netlify/functions/alipay-pay.js
@@ -6,7 +6,7 @@ const alipay = new AlipaySdk({
   appId: process.env.ALIPAY_APP_ID,
   privateKey: process.env.APP_PRIVATE_KEY, // PKCS#8 string with \n
   alipayPublicKey: process.env.ALIPAY_PUBLIC_KEY,
-  gateway: process.env.ALIPAY_GATEWAY || "https://openapi.alipay.com/gateway.do",
+  gateway: "https://openapi.alipaydev.com/gateway.do",
   signType: "RSA2",
 });
 
